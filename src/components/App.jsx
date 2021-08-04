1c345a6e687b50f0bb714dd2119a43f728f272b0
Iryna Chyryk <irachyryk2@gmail.com>
2021-08-04 19:05:56 +0300

    Adding localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,24 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
+
   formHandleSubmit = ({ name, id, number }) => {
     const entry = {
       id: uuidv4(),
